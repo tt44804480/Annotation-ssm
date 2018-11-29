94a7214a9ae2501c99262d582bd59e3fc2140acc
liuliuliu <liuliuliu@liu.com>
2018-11-29 15:20:13 +0800

    添加 分页 插件 pagehelper

--- src/main/java/com/project/dao/TestDao.java
+++ src/main/java/com/project/dao/TestDao.java
@@ -4,8 +4,13 @@ import org.springframework.stereotype.Repository;
 
 import com.project.entity.VillageEntity;
 
+import java.util.List;
+import java.util.Map;
+
 @Repository
 public interface TestDao {
 
 	public VillageEntity queryVillageById(String id);
+
+	public List<Map<String,Object>> querycusById(String id);
 }
