55ba4a0cde5dae6fff4b2a8df8e7d9542ba1a37e
liuliuliu <liuliuliu@liu.com>
2018-11-30 22:34:51 +0800

    添加aop

---
+++ src/main/java/com/project/aop/TestAop1.java
@@ -0,0 +1,26 @@
+package com.project.aop;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+@Aspect
+@Component
+public class TestAop1 {
+
+	@Pointcut("execution(public Object com.project.controller.TestController.test1(String))")
+	public void pointCut(){}
+	
+	@Before("pointCut()")
+	public void before(JoinPoint jp){
+		 HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getRequest();
+		 String name = request.getParameter("name");
+		 System.out.println("aop中的name=  "+name);
+	}
+}
